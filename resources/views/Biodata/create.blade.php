e551a76f7ad8911d751a8569fe99eb0fc4c09897
Divia Anggraini <e31221247@student.polije.ac.id>
2024-09-20 10:27:11 +0800

    Initial commit

---
+++ resources/views/Biodata/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>FORMULIR BIODATA</h1>
+        <form method="POST"action="{{ route('biodata.store') }}">
+            @CSRF
+            <div class="mb-3">
+              <label for="nama" class="form-label">Nama</label>
+              <input type="nama" class="form-control"  aria-describedby="" placeholder="Tambahkan nama" name="nama" required>
+              
+            </div>
+            <div class="mb-3">
+                <label for="keterangan" class="form-label">Keterangan</label>
+                <textarea type="nama" class="form-control"  aria-describedby="" placeholder="Tambahkan keterangan" name="keterangan" required></textarea>
+                <!--<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
+            </div>
+            <div class="mb-3">
+                <label for="usia" class="form-label">Usia</label>
+                <input type="usia" class="form-control"  aria-describedby="" placeholder="Tambahkan usia" name="usia" required>
+                <!--<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
+            </div>
+            <div class="mb-3">
+                <label for="alamat" class="form-label">Alamat</label>
+                <input type="alamat" class="form-control"  aria-describedby="" placeholder="Tambahkan alamat" name="alamat" required>
+                <!--<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+  
+@endsection
