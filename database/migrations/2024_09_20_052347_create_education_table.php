7f520c3f235b0cce3a4bf7e89745991ef26ab1b9
Divia Anggraini <e31221247@student.polije.ac.id>
2024-09-20 13:35:09 +0800

    membuat tabel eduation2

--- database/migrations/2024_09_20_052347_create_education_table.php
+++ database/migrations/2024_09_20_052347_create_education_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('education', function (Blueprint $table) {
             $table->id();
+            $table->string('universitas');
+            $table->string('jurusan');
+            $table->text('minat');
             $table->timestamps();
         });
     }
